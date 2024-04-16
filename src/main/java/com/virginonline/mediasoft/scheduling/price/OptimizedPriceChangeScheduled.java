aa795d40fc5f030364f3e75a0332f2b61963ee84
virginonline <mycrak1337@gmail.com>
2024-04-16 12:53:19 +0700

    :sparkles: feat(OptimizedPriceChangeScheduled): create OptimizedPriceChangeScheduled

--- src/main/java/com/virginonline/mediasoft/scheduling/price/OptimizedPriceChangeScheduled.java
+++ src/main/java/com/virginonline/mediasoft/scheduling/price/OptimizedPriceChangeScheduled.java
@@ -1,10 +1,17 @@
 package com.virginonline.mediasoft.scheduling.price;
 
+import com.virginonline.mediasoft.annotation.Timed;
+import com.virginonline.mediasoft.domain.Product;
 import com.virginonline.mediasoft.scheduling.AbstractPriceChangeScheduled;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -12,6 +19,26 @@ public class OptimizedPriceChangeScheduled extends AbstractPriceChangeScheduled 
 
   private final JdbcTemplate jdbcTemplate;
 
+  @Timed
   @Scheduled(fixedRateString = "${app.scheduling.period}")
-  protected void execute() {}
+  @Transactional(rollbackFor = Exception.class)
+  protected void execute() {
+    var products =
+        jdbcTemplate.query(
+            "select article, price from products", new BeanPropertyRowMapper<>(Product.class));
+    jdbcTemplate.batchUpdate(
+        "update products set price = ?, updated_at = now() where article = ?",
+        new BatchPreparedStatementSetter() {
+          @Override
+          public void setValues(PreparedStatement ps, int i) throws SQLException {
+            ps.setBigDecimal(1, getNewPrice(products.get(i).getPrice()));
+            ps.setLong(2, products.get(i).getArticle());
+          }
+
+          @Override
+          public int getBatchSize() {
+            return products.size();
+          }
+        });
+  }
 }
